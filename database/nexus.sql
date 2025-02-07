debb7c9f8cd20289cf6a37173645ad2aae551b46
Denis <167305961+SkvorikovCode@users.noreply.github.com>
2025-02-08 01:59:43 +0300

    fix: обновлена структура БД, добавлен AUTO_INCREMENT для id

--- database/nexus.sql
+++ database/nexus.sql
@@ -1,120 +1,84 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 9.2.0, for macos15.2 (arm64)
 --
--- Хост: 127.0.0.1
--- Время создания: Фев 07 2025 г., 23:13
--- Версия сервера: 10.4.32-MariaDB
--- Версия PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+03:00";
-
+-- Host: localhost    Database: nexus
+-- ------------------------------------------------------
+-- Server version	9.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- База данных: `nexus`
+-- Table structure for table `news`
 --
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `news`
---
-
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `news` (
-  `id` int(110) NOT NULL,
-  `title` text DEFAULT NULL,
-  `text` text NOT NULL,
-  `images` varchar(250) DEFAULT NULL,
-  `user_id` int(110) NOT NULL,
-  `likes` int(11) NOT NULL,
-  `dislikes` int(11) NOT NULL,
-  `comments` varchar(150) NOT NULL,
-  `images_1` varchar(250) DEFAULT NULL,
-  `images_2` varchar(250) DEFAULT NULL
+  `id` int NOT NULL,
+  `title` text COLLATE utf8mb4_general_ci,
+  `text` text COLLATE utf8mb4_general_ci NOT NULL,
+  `images` varchar(250) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `likes` int NOT NULL,
+  `dislikes` int NOT NULL,
+  `comments` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `images_1` varchar(250) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `images_2` varchar(250) COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Структура таблицы `profile`
+-- Table structure for table `profile`
 --
 
+DROP TABLE IF EXISTS `profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `profile` (
-  `id` int(110) NOT NULL,
-  `user_id` int(110) NOT NULL,
-  `images` varchar(250) NOT NULL,
-  `news_id` int(110) NOT NULL
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `images` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
+  `news_id` int NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Структура таблицы `users`
+-- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(110) NOT NULL,
-  `family` varchar(110) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(110) COLLATE utf8mb4_general_ci NOT NULL,
+  `family` varchar(110) COLLATE utf8mb4_general_ci NOT NULL,
   `age` date NOT NULL,
-  `phone` varchar(20) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `pass` varchar(24) NOT NULL, -- TODO: Я тут изменил, на 255 из-за hash пароля а не того, что дает пользак сайту
-  `profilePic` varchar(250) NOT NULL
+  `phone` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `pass` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `profilePic` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `news`
---
-ALTER TABLE `news`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `profile`
---
-ALTER TABLE `profile`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `news`
---
-ALTER TABLE `news`
-  MODIFY `id` int(110) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `profile`
---
-ALTER TABLE `profile`
-  MODIFY `id` int(110) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-08  1:59:36
