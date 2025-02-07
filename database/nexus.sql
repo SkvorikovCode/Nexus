2b40c9964d25a89fe2fd972859c969125a652ea0
Karikatyra <148780854+Karikatyra@users.noreply.github.com>
2025-02-08 01:28:14 +0300

    Change users.sql & New database profile.sql & news.sql
    Изменен users.sql:
    
    - phone int(change on VARCHAR)
    - added profilePic
    
    Добавлен news.sql:
    
    - id A_I необходимо использование в profile.php (Внутри profile.sql (ниже коммит), будет использоваться как news_id)
    - title
    - text
    
    --------------------------------------------------------------
    - images NULL
    - images_1 NULL
    - images_2 NULL
    
    Блок необязательный к заполнению, но максимальное кол-во фото внутри одного блока новостей.
    --------------------------------------------------------------
    
    - likes
    - dislikes
    - comments
    
    Добавлен profile.sql:
    - id A_I необходимо использование в news.php (Внутри news.sql, будет использоваться как user_id)
    - user_id (id берется при регистрации, возможно будет заменять верхний пункт, пока нет определенности)
    - images (по предварительным данным будет работать условно как в вк, ограничение на показ на изначальной странице <6, если провалиться в будущий файл(images.php??user_id(условно)) будут видны все фото.
    - news_id необходим для показа новостей конкретного человека на личной странице(profile.php).

---
+++ database/nexus.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Фев 07 2025 г., 23:13
+-- Версия сервера: 10.4.32-MariaDB
+-- Версия PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `nexus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(110) NOT NULL,
+  `title` text DEFAULT NULL,
+  `text` text NOT NULL,
+  `images` varchar(250) DEFAULT NULL,
+  `user_id` int(110) NOT NULL,
+  `likes` int(11) NOT NULL,
+  `dislikes` int(11) NOT NULL,
+  `comments` varchar(150) NOT NULL,
+  `images_1` varchar(250) DEFAULT NULL,
+  `images_2` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `profile`
+--
+
+CREATE TABLE `profile` (
+  `id` int(110) NOT NULL,
+  `user_id` int(110) NOT NULL,
+  `images` varchar(250) NOT NULL,
+  `news_id` int(110) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(110) NOT NULL,
+  `family` varchar(110) NOT NULL,
+  `age` date NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `pass` varchar(24) NOT NULL,
+  `profilePic` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(110) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `id` int(110) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
